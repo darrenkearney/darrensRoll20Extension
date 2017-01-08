2a85abfae111cd641cde7e5b2a92eb49bc766b36
Darren Kearney <github@darrenk.net>
2017-01-08 17:32:12 +0000

    Using a switch for commands instead of checking a command list which is redundant. Also added a debug thing.   Changes to be committed: 	modified:   app.js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
  *  This module is used to interface with roll20's chat interface, to recieve input and to output text.
  */
 
+var isDebug = true;
+function debugLog(msg){
+    console.log(msg);
+}
+
 // init stuff
 // reference the text chat container element
 var chatBox = document.getElementById('textchat');
@@ -10,34 +15,36 @@ var chatBox = document.getElementById('textchat');
 messages = chatBox.querySelectorAll('.message.you');
 lastMessage = messages[messages.length-1];
 
-// set up a list of commands ot compare to
-var commandList = [ 'playSound', 'makeTea' ];
-
-function getMessage() {
+function getNewMessage() {
     // Goes through the text chat container and selects the last message
     // if the message is new it returns the message, otherwise it returns false
-
     messages = chatBox.querySelectorAll('.message.you');
+    
     // get the textnode of the last message
     currentMessage = messages[messages.length-1];
+    debugLog('current message: ' + currentMessage);
 
     // get the data off of it and apply str.trim to remove leading and trailing whitespace
     var messageText = currentMessage.lastChild.data.trim();
+    debugLog('message text: ' + messageText);
 
-    // checks the id of the message against the id of the last used message
-    if ( currentMessage.getAttribute('data-messageid') === lastMessage.getAttribute('data-messageid')) {
-        console.log("same message as last message")
-        return false;
+    // checks that triggering message isn't the same id as last message
+    if ( typeof lastMessage !== 'undefined' ) {    
+        // checks the id of the message against the id of the last used message
+        if ( currentMessage.getAttribute('data-messageid') === lastMessage.getAttribute('data-messageid') ) {
+            console.log("same message as last message");
+            return false;
+        }
     }
-    // Print out text content
-    console.log(messageText);
+    
     // The current message has been worked on, let's make sure we don't do that again!
     lastMessage = currentMessage;
+
     // return the string of the entire message
     return messageText;
 }
 
-function parseMessage( messageText ){
+function splitMessage( messageText ){
     var messSplit = messageText.split( ' ' );
     var command = messSplit.shift();
     var arguments = messSplit.join(' ');
@@ -47,22 +54,34 @@ function parseMessage( messageText ){
 // takes an array of [command, arguments]
 function doCommand( commands ) {
 
-    var command = commands[0];
+    var commandName = commands[0];
     var arguments = commands[1];
 
-    // if the command is in the command list then do it
-    if ( commandList.indexOf(command) > -1 ) {
-        console.log("trying command...");
-        console.log("The command: " + command);
-        console.log("The arguments: " + arguments);
+    console.log("Trying command...");
+    console.log("The command: " + commandName);
+    console.log("The arguments: " + arguments);
 
-        if (command === 'playSound') {
-            console.log("You play the mandolin: " + arguments);
-        }
+    switch ( command ) {
+
+        case 'playSound':
+            debugLog("You play the mandolin: " + arguments);
+            break;
+        case 'say':
+            rewriteLastMessage( arguments );
+            break;
+        default:
+            debugLog("Not a command: " + command);
     }
 }
 
+function rewriteLastMessage( msg ) {
+    debugLog(lastMessage);
+    debugLog('say msg: ' + msg);
 
+    lastMessage.innerHTML = msg;
+}
+
+// Observe a specific DOM element
 var observeDOM = (function(){
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
         eventListenerSupported = window.addEventListener;
@@ -74,8 +93,8 @@ var observeDOM = (function(){
                 if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
                     callback();
             });
-            // have the observer observe foo for changes in children
-            obs.observe( obj, { childList:true, subtree:true });
+            // have the observer observe obj for changes in children
+            obs.observe( obj, { childList:true, subtree:true } );
         }
         else if( eventListenerSupported ){
             obj.addEventListener('DOMNodeInserted', callback, false);
@@ -84,15 +103,15 @@ var observeDOM = (function(){
     }
 })();
 
-// Observe a specific DOM element:
+// Start observation
 observeDOM( chatBox, function(){
     console.log('dom changed');
 
-    var messageText = getMessage();
-    console.log("messageText is typeof: " + typeof messageText);
+    var messageText = getNewMessage();
+    debugLog("messageText is typeof: " + typeof messageText);
     if ( typeof messageText === 'string' ) {
         //var commands = parseMessage( messageText );
-        doCommand( parseMessage( messageText ) );
+        doCommand( splitMessage( messageText ) );
     }
 });
 
